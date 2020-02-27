3e52969e676fe4896399cdf7946ee15b10767afc
Harry Turnbull <hturnbull@live.co.uk>
2020-02-27 02:30:24 +0000

    Refactored to class

--- main.rb
+++ main.rb
@@ -1,4 +1,37 @@
-require "pp"
+
+class Boggle
+  def initialize
+    
+    @grid = @@dice.shuffle.map{|die| die.sample}.each_slice(4).to_a
+  end
+  
+  def display
+    puts row = "+-----+-----+-----+-----+"
+    @grid.each do |line|
+      puts "|  #{line.map { |c| c.ljust(2)}.join(" |  ")} |"
+      puts row
+    end
+  end
+
+  @@dice = [
+    ["R", "I", "F", "O", "B", "X"],
+    ["I", "F", "E", "H", "E", "Y"],
+    ["D", "E", "N", "O", "W", "S"],
+    ["U", "T", "O", "K", "N", "D"],
+    ["H", "M", "S", "R", "A", "O"],
+    ["L", "U", "P", "E", "T", "S"],
+    ["A", "C", "I", "T", "O", "A"],
+    ["Y", "L", "G", "K", "U", "E"],
+    ["Qu", "B", "M", "J", "O", "A"],
+    ["E", "H", "I", "S", "P", "N"],
+    ["V", "E", "T", "I", "G", "N"],
+    ["B", "A", "L", "I", "Y", "T"],
+    ["E", "Z", "A", "V", "N", "D"],
+    ["R", "A", "L", "E", "S", "C"],
+    ["U", "W", "I", "L", "R", "G"],
+    ["P", "A", "C", "E", "M", "D"],
+  ]
+end
 
 def roll_dice
   dice = [
@@ -23,19 +56,7 @@ def roll_dice
   dice.shuffle.map{|die| die.sample}.each_slice(4).to_a
 end
 
-def display_grid(grid)
-  puts row = "+-----+-----+-----+-----+"
-  
-  grid.each do |line|
-    puts "|  #{line.map { |c| c.ljust(2)}.join(" |  ")} |"
-    puts row
-  end
-end
+new_game = Boggle.new
+new_game.display
 
-
-
-
-new_rolls = roll_dice
-display_grid(new_rolls)
-
-# => [["F", "U", "S", "D"], ["S", "K", "E", "P"], ["R", "O", "O", "V"], ["I", "Qu", "U", "H"]
+# Example grid => [["F", "U", "S", "D"], ["S", "K", "E", "P"], ["R", "O", "O", "V"], ["I", "Qu", "U", "H"]
