34ffbbc0192b61c20aa489f8226c9eb845ae4fa2
Harry Turnbull <hturnbull@live.co.uk>
2020-02-27 03:51:40 +0000

    rubocop suggested refactors

--- main.rb
+++ main.rb
@@ -1,37 +1,37 @@
 class Boggle
   def initialize
-    @grid = @@dice.shuffle.map{|die| die.sample}.each_slice(4).to_a
+    @grid = @@dice.shuffle.map{ &:sample }.each_slice(4).to_a
   end
 
-def display
-    puts row = "+-----+-----+-----+-----+"
+  def display
+    puts row = '+-----+-----+-----+-----+'
     @grid.each do |line|
-      puts "|  #{line.map { |c| c.ljust(2)}.join(" |  ")} |"
+      puts "|  #{line.map { |c| c.ljust(2)}.join(' |  ')} |"
       puts row
     end
   end
 
   @@dice = [
-    ["R", "I", "F", "O", "B", "X"],
-    ["I", "F", "E", "H", "E", "Y"],
-    ["D", "E", "N", "O", "W", "S"],
-    ["U", "T", "O", "K", "N", "D"],
-    ["H", "M", "S", "R", "A", "O"],
-    ["L", "U", "P", "E", "T", "S"],
-    ["A", "C", "I", "T", "O", "A"],
-    ["Y", "L", "G", "K", "U", "E"],
-    ["Qu", "B", "M", "J", "O", "A"],
-    ["E", "H", "I", "S", "P", "N"],
-    ["V", "E", "T", "I", "G", "N"],
-    ["B", "A", "L", "I", "Y", "T"],
-    ["E", "Z", "A", "V", "N", "D"],
-    ["R", "A", "L", "E", "S", "C"],
-    ["U", "W", "I", "L", "R", "G"],
-    ["P", "A", "C", "E", "M", "D"],
+    ['R', 'I', 'F', 'O', 'B', 'X'],
+    ['I', 'F', 'E', 'H', 'E', 'Y'],
+    ['D', 'E', 'N', 'O', 'W', 'S'],
+    ['U', 'T', 'O', 'K', 'N', 'D'],
+    ['H', 'M', 'S', 'R', 'A', 'O'],
+    ['L', 'U', 'P', 'E', 'T', 'S'],
+    ['A', 'C', 'I', 'T', 'O', 'A'],
+    ['Y', 'L', 'G', 'K', 'U', 'E'],
+    ['Qu', 'B', 'M', 'J', 'O', 'A'],
+    ['E', 'H', 'I', 'S', 'P', 'N'],
+    ['V', 'E', 'T', 'I', 'G', 'N'],
+    ['B', 'A', 'L', 'I', 'Y', 'T'],
+    ['E', 'Z', 'A', 'V', 'N', 'D'],
+    ['R', 'A', 'L', 'E', 'S', 'C'],
+    ['U', 'W', 'I', 'L', 'R', 'G'],
+    ['P', 'A', 'C', 'E', 'M', 'D'],
   ]
 end
 
 new_game = Boggle.new
 new_game.display
 
-# Example grid => [["F", "U", "S", "D"], ["S", "K", "E", "P"], ["R", "O", "O", "V"], ["I", "Qu", "U", "H"]
+# Example grid => [['F', 'U', 'S', 'D'], ['S', 'K', 'E', 'P'], ['R', 'O', 'O', 'V'], ['I', 'Qu', 'U', 'H']
