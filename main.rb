a0d54fbd17801cfdbc792720db430029da08e32f
Harry Turnbull <hturnbull@live.co.uk>
2020-02-28 18:51:53 +0000

    changes array of dice to %w array

--- main.rb
+++ main.rb
@@ -12,22 +12,22 @@ class Boggle
   end
 
   @@dice = [
-    ['R', 'I', 'F', 'O', 'B', 'X'],
-    ['I', 'F', 'E', 'H', 'E', 'Y'],
-    ['D', 'E', 'N', 'O', 'W', 'S'],
-    ['U', 'T', 'O', 'K', 'N', 'D'],
-    ['H', 'M', 'S', 'R', 'A', 'O'],
-    ['L', 'U', 'P', 'E', 'T', 'S'],
-    ['A', 'C', 'I', 'T', 'O', 'A'],
-    ['Y', 'L', 'G', 'K', 'U', 'E'],
-    ['Qu', 'B', 'M', 'J', 'O', 'A'],
-    ['E', 'H', 'I', 'S', 'P', 'N'],
-    ['V', 'E', 'T', 'I', 'G', 'N'],
-    ['B', 'A', 'L', 'I', 'Y', 'T'],
-    ['E', 'Z', 'A', 'V', 'N', 'D'],
-    ['R', 'A', 'L', 'E', 'S', 'C'],
-    ['U', 'W', 'I', 'L', 'R', 'G'],
-    ['P', 'A', 'C', 'E', 'M', 'D'],
+    %w(R I F O B X),
+    %w(I F E H E Y),
+    %w(D E N O W S),
+    %w(U T O K N D),
+    %w(H M S R A O),
+    %w(L U P E T S),
+    %w(A C I T O A),
+    %w(Y L G K U E),
+    %w(Qu B M J O A),
+    %w(E H I S P N),
+    %w(V E T I G N),
+    %w(B A L I Y T),
+    %w(E Z A V N D),
+    %w(R A L E S C),
+    %w(U W I L R G),
+    %w(P A C E M D),
   ]
 end
 
