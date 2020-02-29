d2997d819a790cebd26cfb059fd9ef9bd5a4feb4
Harry Turnbull <hturnbull@live.co.uk>
2020-02-29 03:14:42 +0000

    finished refactoring timed word input

--- main.rb
+++ main.rb
@@ -2,72 +2,71 @@ require 'io/console'
 
 class Boggle
   def initialize
-    @grid = @@dice.shuffle.map{ |die| die.sample }.each_slice(4).to_a
+    @grid = @@dice.shuffle.map(&:sample).each_slice(4).to_a
   end
 
   def display
     puts row = '+-----+-----+-----+-----+'
     @grid.each do |line|
-      puts "|  #{line.map { |c| c.ljust(2)}.join(' |  ')} |"
+      puts "|  #{line.map { |c| c.ljust(2) }.join(' |  ')} |"
       puts row
     end
   end
 
   @@dice = [
-    %w(R I F O B X),
-    %w(I F E H E Y),
-    %w(D E N O W S),
-    %w(U T O K N D),
-    %w(H M S R A O),
-    %w(L U P E T S),
-    %w(A C I T O A),
-    %w(Y L G K U E),
-    %w(Qu B M J O A),
-    %w(E H I S P N),
-    %w(V E T I G N),
-    %w(B A L I Y T),
-    %w(E Z A V N D),
-    %w(R A L E S C),
-    %w(U W I L R G),
-    %w(P A C E M D),
+    %w[R I F O B X],
+    %w[I F E H E Y],
+    %w[D E N O W S],
+    %w[U T O K N D],
+    %w[H M S R A O],
+    %w[L U P E T S],
+    %w[A C I T O A],
+    %w[Y L G K U E],
+    %w[Qu B M J O A],
+    %w[E H I S P N],
+    %w[V E T I G N],
+    %w[B A L I Y T],
+    %w[E Z A V N D],
+    %w[R A L E S C],
+    %w[U W I L R G],
+    %w[P A C E M D]
   ]
 end
 
-
-
 def word_input
-  
   words = []
-  
-  puts "Type in your words, press enter to submit."
-  
-  still_time = true
 
-  timer = Thread.new {
+  puts 'Type in your words, press enter to submit.'
+
+  player_input = Thread.new do
+
     time = 5
-    while time > 0
-      sleep 1
-      time -=1
+
+    timer = Thread.new do
+      while time > 0
+        sleep 1
+        time -= 1
+      end
+      puts "\nTime's up!"
+      player_input.exit
     end
-    still_time = false
-  }
-  
-  while still_time
-    input = gets.chomp
-    if input == '!'
-      timer.exit
-      break
-    else
-      words.push(input)
+    
+    loop do
+      input = gets.chomp
+      if input == '!'
+        timer.exit
+        puts "You quit early with #{time} seconds to go."
+        break
+      else
+        words.push(input)
+      end
     end
+
   end
-  
-  timer.join
 
-  puts "Time's up! Finish your last word."
+  player_input.join
 
-  
-  puts "Your words are:"
+  puts 'Your words are:'
   puts words.join(', ')
   
 end
@@ -76,61 +75,53 @@ new_game = Boggle.new
 new_game.display
 word_input
 
-
-
-
-=begin 
-
-def gets_with_quit
-  char = ''
-  string = ''
-  while true do 
-    char = STDIN.getch
-    if char.ord == 27
-      return false
-    end
-    if char.ord == 13
-      puts
-      return string
-    end
-    print char
-    string += char
-  end
-end
-
-
-def word_input
-  words = []
-
-  puts "Type in your words, press enter to submit."
-
-
-  while true
-    input = gets_with_quit
-
-    if input == false
-      puts; puts "Are you sure you want to quit? y/n"
-
-      while true
-        case STDIN.noecho(&:getch)
-        when 'y'
-          puts; puts 'Your words are:'
-          p words.join(', ')
-          return words
-        when "n"
-          puts "Continue"
-          break
-        end
-      end
-  
-    else
-      words.push(input.split)
-    end
-
-  end
-end
-=end
-
+#
+# def gets_with_quit
+#   char = ''
+#   string = ''
+#   while true do
+#     char = STDIN.getch
+#     if char.ord == 27
+#       return false
+#     end
+#     if char.ord == 13
+#       puts
+#       return string
+#     end
+#     print char
+#     string += char
+#   end
+# end
+#
+# def word_input
+#   words = []
+#
+#   puts "Type in your words, press enter to submit."
+#
+#   while true
+#     input = gets_with_quit
+#
+#     if input == false
+#       puts; puts "Are you sure you want to quit? y/n"
+#
+#       while true
+#         case STDIN.noecho(&:getch)
+#         when 'y'
+#           puts; puts 'Your words are:'
+#           p words.join(', ')
+#           return words
+#         when "n"
+#           puts "Continue"
+#           break
+#         end
+#       end
+#
+#     else
+#       words.push(input.split)
+#     end
+#
+#   end
+# end
 
 # Example grid => [['F', 'U', 'S', 'D'], ['S', 'K', 'E', 'P'], ['R', 'O', 'O', 'V'], ['I', 'Qu', 'U', 'H']
 
